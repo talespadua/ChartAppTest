537f1c6f89f952ff3a518511c876094b3ff8cf3e
talespadua <talescpadua@gmail.com>
2015-10-22 23:08:27 -0200

    First Hello World commit

---
+++ app/src/main/java/com/sherol/tales/chartapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sherol.tales.chartapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToMain(View v){
+        Intent i =  new Intent(getApplicationContext(), ChartActivity.class);
+        startActivity(i);
+    }
+}
